85a86a04fc39ae7faa016bad2a68a724b1d88966
Sjors Spoorendonk <sjors@vicompany.nl>
2019-09-12 08:29:25 +0200

    CSS minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
-module.exports = function(config) {
+const CleanCSS = require('clean-css');
+
+module.exports = function (config) {
 
 	// LAYOUTS
 	config.addLayoutAlias('base', 'layouts/base.njk');
 	config.addLayoutAlias('home', 'layouts/homepage.njk');
 	config.addLayoutAlias('tour', 'layouts/tour.njk');
+
+	// Minify
+	config.addFilter('cssmin', function(code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
 	// pass some assets right through
 	config.addPassthroughCopy('source/css');
