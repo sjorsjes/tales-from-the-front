a034d6f9a1ba58d46ffee7cd2a362fcef965bfcf
Sjors Spoorendonk <sjors@vicompany.nl>
2019-10-07 19:24:55 +0200

    Responsive homepage

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,19 @@
-const cssnano = require('cssnano');
-const customProperties = require('postcss-custom-properties');
 const atImport = require('postcss-import');
+const mediaVariables = require('postcss-media-variables');
+const customProperties = require('postcss-custom-properties');
+const customMedia = require('postcss-custom-media');
 const mqPacker = require('css-mqpacker');
+const cssnano = require('cssnano');
 
 module.exports = {
 	plugins: [
 		atImport(),
+		mediaVariables(),
+		customMedia(),
 		customProperties({
 			preserve: false,
 		}),
+		mediaVariables(),
 		mqPacker(),
 		cssnano({
 			preset: 'default',
