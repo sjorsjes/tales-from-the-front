00739de04a98ffb490ecb953f753a411b3990b04
Sjors Spoorendonk <sjors@vicompany.nl>
2019-10-28 16:02:00 +0100

    Updated to Nunchunks

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,26 @@
 module.exports = function (config) {
 
 	// LAYOUTS
-	config.addLayoutAlias('base', 'layouts/base.ejs');
-	config.addLayoutAlias('home', 'layouts/homepage.ejs');
-	config.addLayoutAlias('tour', 'layouts/tour.ejs');
+	config.addLayoutAlias('base', 'layouts/base.njk');
+	config.addLayoutAlias('home', 'layouts/homepage.njk');
+	config.addLayoutAlias('tour', 'layouts/tour.njk');
+
+	config.addCollection('upcomingTour', function (collection) {
+		const col = collection.getFilteredByTag('tours');
+		const last = col[col.length - 1];
+		const hasUpcomingTour = Date.now() < new Date(last.data.date).getTime();
+
+		return hasUpcomingTour ? [last] : [];
+	});
+
+	config.addFilter('formatDate', function (value) {
+		const date = new Date(value);
+		const day = ('0' + date.getDate()).slice(-2);
+		const month = ('0' + (date.getMonth() + 1)).slice(-2);
+		const year = date.getFullYear();
+
+		return `${day}-${month}-${year}`;
+	});
 
 	config.addPassthroughCopy('./source/img');
 	config.addPassthroughCopy('./source/css/**/*.min.css');
--- source/index.md
+++ source/index.md
@@ -2,4 +2,6 @@
 layout: home
 title: Tales from the front
 subtitle: Stap in en beleef een reis naar betekenisvolle momenten in de geschiedenis op één van onze Battlefield Tours. Ontdek en gedenk de strijd die nodig was voor onze vrijheid…
+tags: nav
+navtitle: home
 ---
