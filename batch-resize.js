19c9098998c72660148fa7dbc19aac7709d83945
Sjors Spoorendonk <sjors@vicompany.nl>
2022-01-22 20:22:56 +0100

    Better image resize

--- batch-resize.js
+++ batch-resize.js
@@ -3,34 +3,30 @@ const sharp = require('sharp');
 
 const inputDirectory = './source/img/visuals';
 const outputDirectory = './dist/img/visuals';
-const sizes = [null, 320, 1100, 1650, 1980];
 
+const fileSizes = [null, 320, 1100, 1650, 1980];
 const files = fs.readdirSync(inputDirectory);
 
-console.log(files);
-
 const processImages = async () => {
-	const metadata = await sharp(`${inputDirectory}/${files[0]}`).metadata();
+	const resizePromises = [];
 
-	console.log(metadata);
+	files.forEach((fileName) => {
+		fileSizes.forEach((fileSize) => {
+			resizePromises.push(resizeImage(fileName, fileSize));
+		});
+	})
+
+	await Promise.allSettled(resizePromises);
+}
+
+const resizeImage = async (fileName, fileSize) => {
+	const name = fileSize
+		? fileName.replace(/(\.[\w\d_-]+)$/i, `-${fileSize}w$1`)
+		: fileName;
+
+	await sharp(`${inputDirectory}/${fileName}`)
+		.resize({ width: fileSize })
+		.toFile(`${outputDirectory}/${name}`);
 }
 
 processImages();
-
-// sharp.cache({ files : 0 });
-
-// fs.readdirSync(inputDirectory)
-// 	.forEach(async (file) => {
-// 		return Promise.all(
-// 			sizes.map((size, i) =>{
-// 				if (!size) {
-// 					return sharp(`${inputDirectory}/${file}`)
-// 						.toFile(`${outputDirectory}/${file}.jpg`);
-// 				}
-
-// 				return sharp(`${inputDirectory}/${file}`)
-// 					.resize({ width: size })
-// 					.toFile(`${outputDirectory}/${file}-${size}.jpg`);
-// 			})
-// 		);
-// 	});
