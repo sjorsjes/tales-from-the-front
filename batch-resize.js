175d3fe8e4c4c5e34e1f2d5d4d78e1f4da110268
Sjors Spoorendonk <sjors@vicompany.nl>
2022-01-22 21:05:32 +0100

    Add dir of not there

--- batch-resize.js
+++ batch-resize.js
@@ -7,6 +7,12 @@ const outputDirectory = './dist/img/visuals';
 const fileSizes = [null, 320, 1100, 1650, 1980];
 const files = fs.readdirSync(inputDirectory);
 
+const checkDir = (dir) => {
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir, { recursive: true });
+	}
+}
+
 const resizeImage = async (fileName, fileSize) => {
 	const name = fileSize
 		? fileName.replace(/(\.[\w\d_-]+)$/i, `-${fileSize}w$1`)
@@ -21,6 +27,8 @@ const resizeImage = async (fileName, fileSize) => {
 
 const processImages = async () => {
 	const resizePromises = [];
+
+	checkDir(outputDirectory);
 
 	files.forEach((fileName) => {
 		fileSizes.forEach((fileSize) => {
