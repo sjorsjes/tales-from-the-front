8b46384ffc73094ef0e0534999e50b3da9df7ef7
Sjors Spoorendonk <sjors@vicompany.nl>
2022-01-22 21:15:26 +0100

    check dir

--- batch-resize.js
+++ batch-resize.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
 
-const inputDirectory = './source/img/visuals';
-const outputDirectory = './dist/img/visuals';
+const inputDirectory = path.join(__dirname, './source/img/visuals');
+const outputDirectory = path.join(__dirname, './dist/img/visuals');
 
 const fileSizes = [null, 320, 1100, 1650, 1980];
 const files = fs.readdirSync(inputDirectory);
@@ -12,10 +12,6 @@ const checkDir = (dir) => {
 	if (!fs.existsSync(dir)) {
 		fs.mkdirSync(dir, { recursive: true });
 	}
-
-	console.log(__dirname);
-	console.log(process.cwd());
-	console.log(path.join(__dirname, outputDirectory))
 }
 
 const resizeImage = async (fileName, fileSize) => {
